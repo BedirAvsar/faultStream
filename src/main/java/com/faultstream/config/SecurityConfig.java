9fbb9e2dadde292844f93310ae730e545e153b5b
Bedir Avsar <bediravsar@gmail.com>
2026-03-28 08:39:59 +0300

    security config hataları fixlendi ve derlemeler temizlendi

---
+++ src/main/java/com/faultstream/config/SecurityConfig.java
@@ -0,0 +1,86 @@
+package com.faultstream.config;
+
+import com.faultstream.security.JwtAuthFilter;
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+@EnableMethodSecurity
+public class SecurityConfig {
+    private final JwtAuthFilter jwtAuthFilter;
+    private final UserDetailsService userDetailsService;
+
+    // SECURITY
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+
+                        .requestMatchers("/api/v1/auth/**").permitAll()
+                        // Sadece login ve register ekranları açık
+                        .anyRequest().authenticated()
+                // Kalan sayfalara jwt tokeni zorunlu!
+                )
+                .sessionManagement(sess -> sess
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                // Sunucu hafızasında kullanıcı tutmayacak, her istekte JWT bileti kontrol
+                // edilecek
+                )
+                .authenticationProvider(authenticationProvider())
+                // Veri tabanı bağlantısı
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
+
+    // ENCRYPTION
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    // AUTHENTICATION
+    @Bean
+    @SuppressWarnings("deprecation")
+    public AuthenticationProvider authenticationProvider() {
+
+        /*
+         * Peki neden?
+         * Çünkü Spring Security "Ben kimim?" diye soruyor
+         * 1. UserDetailsService: "Benim veritabanı bağlantım nerede?"
+         * 2. PasswordEncoder: "Ben şifreleri nasıl çözerim?"
+         * 3. AuthenticationProvider: "Ben kullanıcıyı nasıl doğrulayacağım?"
+         * İŞTE BURASI BU KARARI VEREN YER
+         */
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService);
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
+    }
+
+    // AUTH MANAGER
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+
+}
